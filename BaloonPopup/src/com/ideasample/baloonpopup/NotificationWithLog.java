277a6fd5fb42b148fdd1176955f093d45b61634e
Kasun Gunasekara <kasun32@gmail.com>
2015-06-09 13:45:36 +0530

    removing the deprecated method usage and improving the notification

--- BaloonPopup/src/com/ideasample/baloonpopup/NotificationWithLog.java
+++ BaloonPopup/src/com/ideasample/baloonpopup/NotificationWithLog.java
@@ -1,26 +1,31 @@
 package com.ideasample.baloonpopup;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
+import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
 
-/**
- * Created by kasun32 on 6/4/15.
- */
 public class NotificationWithLog extends AnAction {
+
+	public static final NotificationGroup GROUP_DISPLAY_ID_INFO =
+			new NotificationGroup(PluginConstants.GROUP_NOTIFICATION_ID, NotificationDisplayType.BALLOON, true);
 
 	@Override
 	public void actionPerformed(AnActionEvent e) {
+		Project project = e.getData(PlatformDataKeys.PROJECT);
+		notify(project, PluginConstants.CONNECTION_SUCCESS_TITLE, PluginConstants.CONNECTION_SUCCESS_DESCRIPTION);
+	}
 
-		Notifications.Bus
-				.register(PluginConstants.GROUP_NOTIFICATION_ID, NotificationDisplayType.BALLOON);
-		Notification notification = new Notification(PluginConstants.GROUP_NOTIFICATION_ID,
-		                                             PluginConstants.CONNECTION_SUCCESS_TITLE,
-		                                             PluginConstants.CONNECTION_SUCCESS_DESCRIPTION,
-		                                             NotificationType.INFORMATION);
-		Notifications.Bus.notify(notification);
+	public static void notify(final Project project,String title, String content) {
+		final Notification notification =
+                GROUP_DISPLAY_ID_INFO.createNotification(title, content, NotificationType.INFORMATION, null);
+		ApplicationManager.getApplication().invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Notifications.Bus.notify(notification, project);
+			}
+		});
 	}
 }
