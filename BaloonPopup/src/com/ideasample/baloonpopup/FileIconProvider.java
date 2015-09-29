e08dd919461feeaa9408eb362e5c92e87ee30d74
Holger Schmeisky <google@schmeisky.com>
2015-09-29 14:19:12 +0200

    tests for fileIconProvider

--- BaloonPopup/src/com/ideasample/baloonpopup/FileIconProvider.java
+++ BaloonPopup/src/com/ideasample/baloonpopup/FileIconProvider.java
@@ -14,13 +14,21 @@ import javax.swing.*;
 public class FileIconProvider extends IconProvider {
 
     private static final String INUSE_ICON_URL = "/inuseIcon.png";
+    private static final String OTHER_ICON_URL = "/otherIcon.png";
+
+    public static boolean otherIcon = true;
 
     @Nullable
     @Override
     public Icon getIcon(PsiElement element, int flags) {
         PsiFile containingFile = element.getContainingFile();
-        if (containingFile != null && containingFile.getName().endsWith(".java")) {
-            return IconLoader.getIcon(INUSE_ICON_URL);
+
+        if (containingFile != null) {
+            System.out.println("get Icon: " + element.getContainingFile().getName());
+            if (otherIcon)
+                return IconLoader.getIcon(INUSE_ICON_URL);
+            else
+                return IconLoader.getIcon(OTHER_ICON_URL);
         }
         return null;
     }
